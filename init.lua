9428bfd4e271e1f27ed2026330e3da69a3d94fb3
nicomiguelino <nicomiguelino2014@gmail.com>
2023-03-04 23:41:03 -0800

    Configure Telescope.

--- init.lua
+++ init.lua
@@ -5,3 +5,11 @@ local plugins = require('plugins')
 general.init()
 autocommands.init()
 plugins.init()
+
+-- Telescope configuration
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
