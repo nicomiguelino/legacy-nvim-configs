270732ae58a28ea72d7607c2b6e1781b68de1503
nicomiguelino <nicomiguelino2014@gmail.com>
2023-03-06 11:06:08 -0800

    Add keymapping for disabling text highlight.

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,14 +1,20 @@
 local module = {}
 local set_keymap = vim.api.nvim_set_keymap
 
-function module.init()
-    set_keymap('n', '<leader>w', '<C-w>', {noremap = true})
+local function init_tab_keymaps()
     set_keymap('n', '<leader>to', ':tabnew<CR>', {noremap = true})
     set_keymap('n', '<leader>tx', ':tabclose<CR>', {noremap = true})
     set_keymap('n', '<leader>tj', ':tabprevious<CR>', {noremap = true})
     set_keymap('n', '<leader>tl', ':tabnext<CR>', {noremap = true})
     set_keymap('n', '<leader>tk', ':+tabmove<CR>', {noremap = true})
     set_keymap('n', '<leader>ti', ':-tabmove<CR>', {noremap = true})
+end
+
+function module.init()
+    set_keymap('n', '<leader>w', '<C-w>', {noremap = true})
+    init_tab_keymaps()
+
+    set_keymap('n', '<leader>hn', ':nohlsearch<CR>')
 end
 
 return module
