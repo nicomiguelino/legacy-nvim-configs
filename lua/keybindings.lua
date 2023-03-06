f666476c0c01bb7f067d909d50589a7bd97a65e9
nicomiguelino <nicomiguelino2014@gmail.com>
2023-03-06 11:13:34 -0800

    Modify tab movement key mappings.

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -4,10 +4,10 @@ local set_keymap = vim.api.nvim_set_keymap
 local function init_tab_keymaps()
     set_keymap('n', '<leader>to', ':tabnew<CR>', {noremap = true})
     set_keymap('n', '<leader>tx', ':tabclose<CR>', {noremap = true})
-    set_keymap('n', '<leader>tj', ':tabprevious<CR>', {noremap = true})
+    set_keymap('n', '<leader>th', ':tabprevious<CR>', {noremap = true})
     set_keymap('n', '<leader>tl', ':tabnext<CR>', {noremap = true})
-    set_keymap('n', '<leader>tk', ':+tabmove<CR>', {noremap = true})
-    set_keymap('n', '<leader>ti', ':-tabmove<CR>', {noremap = true})
+    set_keymap('n', '<leader>tj', ':+tabmove<CR>', {noremap = true})
+    set_keymap('n', '<leader>tk', ':-tabmove<CR>', {noremap = true})
 end
 
 function module.init()
