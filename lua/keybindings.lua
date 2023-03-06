f80074c13f04f7861cf4c6ee6bca94e18a055735
nicomiguelino <nicomiguelino2014@gmail.com>
2023-03-05 22:10:52 -0800

    Add some more keybindings.

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -3,6 +3,10 @@ local set_keymap = vim.api.nvim_set_keymap
 
 function module.init()
     set_keymap('n', '<leader>w', '<C-w>', {noremap = true})
+    set_keymap('n', '<leader>to', ':tabnew<CR>', {noremap = true})
+    set_keymap('n', '<leader>tx', ':tabclose<CR>', {noremap = true})
+    set_keymap('n', '<leader>tj', ':tabprevious<CR>', {noremap = true})
+    set_keymap('n', '<leader>tl', ':tabnext<CR>', {noremap = true})
 end
 
 return module
