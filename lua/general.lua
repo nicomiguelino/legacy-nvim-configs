4c74acdb0076d8c01267cbf181ed8d9217618e1b
nicomiguelino <nicomiguelino2014@gmail.com>
2023-03-06 20:46:21 -0800

    Fix syntax errors.

--- lua/general.lua
+++ lua/general.lua
@@ -2,23 +2,23 @@ local module = {}
 
 function set_global_options()
     local options = {
-        relativenumber = true
-        number = true
-        hlsearch = true
-        hidden = true
-        incsearch = true
-        visualbell = false
-        ignorecase = true
-        swapfile = false
-        expandtab = true
-        tabstop = 4
-        softtabstop = 4
-        shiftwidth = 4
-        shiftround = true
-        autoindent = true
-        copyindent = true
-        smarttab = true
-        cursorline = true
+        relativenumber = true,
+        number = true,
+        hlsearch = true,
+        hidden = true,
+        incsearch = true,
+        visualbell = false,
+        ignorecase = true,
+        swapfile = false,
+        expandtab = true,
+        tabstop = 4,
+        softtabstop = 4,
+        shiftwidth = 4,
+        shiftround = true,
+        autoindent = true,
+        copyindent = true,
+        smarttab = true,
+        cursorline = true,
     }
 
     for k, v in pairs(options) do
