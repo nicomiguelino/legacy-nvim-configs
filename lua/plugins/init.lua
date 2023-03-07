4789756bfc943e810d9f8f87e4ed13fd35357e3c
nicomiguelino <nicomiguelino2014@gmail.com>
2023-03-06 17:37:08 -0800

    Remove `vim-polyglot`.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -27,7 +27,6 @@ function module.init()
         'crusoexia/vim-monokai',
         'nvim-lualine/lualine.nvim',
         'nvim-tree/nvim-web-devicons',
-        'sheerun/vim-polyglot',
     })
 
     telescope_config.init()
