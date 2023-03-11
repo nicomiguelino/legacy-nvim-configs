6da6bb0f96a84976715e1ef6492cd6666a92f26f
nicomiguelino <nicomiguelino2014@gmail.com>
2023-03-11 06:51:59 +0000

    Fix syntax and import errors.

--- lua/plugins/telescope_config.lua
+++ lua/plugins/telescope_config.lua
@@ -3,7 +3,8 @@ local module = {}
 function module.init()
     local builtin = require('telescope.builtin')
     local telescope = require('telescope')
-    local bookmarks = telescope.load_extension('vim_bookmarks')
+    telescope.load_extension('vim_bookmarks')
+    local bookmarks = telescope.extensions.vim_bookmarks
 
     vim.keymap.set('n', '<leader><space>', builtin.find_files, {})
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
