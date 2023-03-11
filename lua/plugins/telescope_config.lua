b0cdd364fd03b03a9a2b083db71301f334f974de
nicomiguelino <nicomiguelino2014@gmail.com>
2023-03-11 07:13:07 +0000

    Refactor further.

--- lua/plugins/telescope_config.lua
+++ lua/plugins/telescope_config.lua
@@ -4,6 +4,7 @@ function module.init()
     local builtin = require('telescope.builtin')
     local telescope = require('telescope')
     telescope.load_extension('vim_bookmarks')
+    local bookmarks = telescope.extensions.vim_bookmarks
 
     vim.keymap.set('n', '<leader><space>', builtin.find_files, {})
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
@@ -12,7 +13,8 @@ function module.init()
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
     -- Bookmarks
-    vim.keymap.set('n', '<leader>ba', telescope.extensions.vim_bookmarks.all, {})
+    -- vim.keymap.set('n', '<leader>ba', telescope.extensions.vim_bookmarks.all, {})
+    vim.keymap.set('n', '<leader>ba', bookmarks.all, {})
 end
 
 return module
