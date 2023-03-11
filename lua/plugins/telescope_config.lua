d5a65bd165905c02afee7861568662e1e980af51
nicomiguelino <nicomiguelino2014@gmail.com>
2023-03-11 06:45:45 +0000

    Integrate bookmarks with Telescope.

--- lua/plugins/telescope_config.lua
+++ lua/plugins/telescope_config.lua
@@ -2,11 +2,18 @@ local module = {}
 
 function module.init()
     local builtin = require('telescope.builtin')
+    local telescope = require('telescope')
+    local bookmarks = telescope.load_extension('vim_bookmarks')
+
     vim.keymap.set('n', '<leader><space>', builtin.find_files, {})
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+    -- Bookmarks
+    vim.keymap.set('n', '<leader>ba', bookmarks.all(), {})
+    vim.keymap.set('n', '<leader>bf', bookmarks.current_file(), {})
 end
 
 return module
