b79f9d14f26a167e712414dee5d03a0c92240590
nicomiguelino <nicomiguelino2014@gmail.com>
2023-10-07 11:16:58 -0700

    fix: Allow hidden files to be visible when searching the file tree.

--- lua/plugins/telescope_config.lua
+++ lua/plugins/telescope_config.lua
@@ -1,12 +1,27 @@
 local module = {}
 
+function get_wrapped_find_files_function(builtin)
+    function lambda()
+        builtin.find_files({
+            hidden = true,
+        })
+    end
+
+    return lambda
+end
+
 function module.init()
     local builtin = require('telescope.builtin')
     local telescope = require('telescope')
     telescope.load_extension('vim_bookmarks')
     local bookmarks = telescope.extensions.vim_bookmarks
 
-    vim.keymap.set('n', '<leader><space>', builtin.find_files, {})
+    vim.keymap.set(
+        'n',
+        '<leader><space>',
+        get_wrapped_find_files_function(builtin),
+        {}
+    )
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
