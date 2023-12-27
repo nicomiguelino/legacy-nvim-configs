62dc463210b914d3c607a03355cb06f448be6bc6
nicomiguelino <nicomiguelino2014@gmail.com>
2023-12-26 17:07:56 -0800

    dependency: Bump `telescope.nvim` from `0.1.1` to `0.1.5`.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,7 +20,7 @@ function module.init()
 
     require('lazy').setup({
         {
-            'nvim-telescope/telescope.nvim', tag = '0.1.1',
+            'nvim-telescope/telescope.nvim', tag = '0.1.5',
             dependencies = { 'nvim-lua/plenary.nvim' }
         },
         'github/copilot.vim',
